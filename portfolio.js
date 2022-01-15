0fea77c8ac5ade7c0aa858a35cb887094a4290db
JadugarKalakaboola <sid.sharma1704@gmail.com>
2022-01-15 21:16:51 +0530

    fixed the nav bar

--- portfolio.js
+++ portfolio.js
@@ -36,3 +36,50 @@ themeBtn.addEventListener("click", changeTheme)
 if(localStorage.getItem("theme") ==="dark"){
     addDarkMode()
 }
+
+const aboutBtn = document.getElementById("about-btn")
+const projectBtn = document.getElementById("project-btn")
+const contactBtn = document.getElementById("contact-btn")
+const homeBtn = document.getElementById("name")
+const aboutSection = document.querySelector(".about")
+const projectSection = document.querySelector(".project-section")
+const contactSection = document.querySelector(".contact-section")
+const homeSection = document.querySelector(".home-section")
+
+aboutBtn.addEventListener("click", function(){
+    homeSection.classList.remove("active")
+    aboutSection.classList.add("active")
+    projectSection.classList.remove("active")
+    contactSection.classList.remove("active")
+    aboutBtn.textContent = "About ▼"
+    projectBtn.textContent = "Project"
+    contactBtn.textContent = "Contact"
+})
+
+projectBtn.addEventListener("click", function(){
+    homeSection.classList.remove("active")
+    aboutSection.classList.remove("active")
+    projectSection.classList.add("active")
+    contactSection.classList.remove("active")
+    aboutBtn.textContent = "About"
+    projectBtn.textContent = "Project ▼"
+    contactBtn.textContent = "Contact"
+})
+contactBtn.addEventListener("click", function(){
+    homeSection.classList.remove("active")
+    aboutSection.classList.remove("active")
+    projectSection.classList.remove("active")
+    contactSection.classList.add("active")
+    aboutBtn.textContent = "About"
+    projectBtn.textContent = "Project"
+    contactBtn.textContent = "Contact ▼"
+})
+homeBtn.addEventListener("click", function(){
+    homeSection.classList.add("active")
+    aboutSection.classList.remove("active")
+    projectSection.classList.remove("active")
+    contactSection.classList.remove("active")
+    aboutBtn.textContent = "About"
+    projectBtn.textContent = "Project"
+    contactBtn.textContent = "Contact"
+})
